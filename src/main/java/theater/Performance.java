293aae5ae1052e92027be7eb6274e98e2e1fbb38
batur340 <baturaslan4@gmail.com>
2025-11-24 00:06:37 -0500

    Task 2.1: Extract getAmount

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,25 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Represents a performance of a play.
+ *
+ * @null no fields are expected to be null
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private final String playID;
+    private final int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
+    }
+
+    public String getPlayID() {
+        return playID;
+    }
+
+    public int getAudience() {
+        return audience;
     }
 }
